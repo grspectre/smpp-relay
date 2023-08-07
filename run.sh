c8db847a472552615f9c94c5de7b476c45b9780c
Anton A Shanaurin <grspectre@gmail.com>
2023-08-07 17:45:33 +0300

    Unnecessary output in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 FULL_PATH="$SCRIPT_PATH/smpp-gateway"
 
-echo $FULL_PATH
 command() {
   $FULL_PATH >output.log 2>error.log &
   echo $! >> smpp-gateway.pid
